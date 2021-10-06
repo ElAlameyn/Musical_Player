aaa2ca8ddd738416040a01c46da5baabdc3bfa14
Artiom Kalinklin <artem.k.3008@gmail.com>
2021-10-06 21:20:49 +0300

    Initial Commit

---
+++ Musical_Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Musical_Player
+//
+//  Created by Артем Калинкин on 06.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
